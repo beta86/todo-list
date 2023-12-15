7214d87fd901c56a957f38332f5f8cd54e484e72
beta86 <8btaylor6@gmail.com>
2023-12-15 16:24:55 +0000

    Remove button component - tailwind issue

--- src/app/page.js
+++ src/app/page.js
@@ -40,9 +40,14 @@ export default function Home() {
           />
 
           <div>
-            <Button type="submit" onClick={handleButtonClick}>
-              Add Item
-            </Button>
+            <div className="m-4">
+              <button
+                type="submit"
+                className="bg-purple-900 text-white px-4 py-2 rounded"
+                onClick={handleButtonClick}>
+                Add Item
+              </button>
+            </div>
           </div>
         </Card>
       </div>
